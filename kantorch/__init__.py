327352f8802874242bc8695a02939a2e17bfe7f7
numerical-engine <syghabc@gmail.com>
2026-06-12 15:12:51 +0900

    Import WavKAN in __init__.py

--- kantorch/__init__.py
+++ kantorch/__init__.py
@@ -2,3 +2,4 @@ from kantorch.fastkan import FastKAN
 from kantorch.relukan import ReLUKAN
 from kantorch.kan import KAN
 from kantorch.fourierkan import FourierKAN
+from kantorch.wavkan import WavKAN
